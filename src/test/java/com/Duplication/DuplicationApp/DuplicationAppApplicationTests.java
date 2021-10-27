c82976b7c2417d3d6e6dc4123ca9d2c345ec5071
kholoodi11 <kholoodi111@outlook.com>
2021-10-27 18:42:02 +0000

    udate test

--- src/test/java/com/Duplication/DuplicationApp/DuplicationAppApplicationTests.java
+++ src/test/java/com/Duplication/DuplicationApp/DuplicationAppApplicationTests.java
@@ -1,20 +1,19 @@
 package com.Duplication.DuplicationApp;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DuplicationAppApplicationTests {
 
-	public DuplicationAppApplication duplicationTest = new DuplicationAppApplication();
+	private DuplicationAppApplication duplicationTest = new DuplicationAppApplication();
 	  @Test
-                Public  void testDoublication()
-                {
-
-                        Integer actual = duplicationTest.Duplication(6);
-                        Integer expected = 12;
-                        assertEquals(expected, actual);
+	  Public void testDoublication()
+	  {
+		  Integer actual = duplicationTest.Duplication(6);
+		  Integer expected = 12;
+		   assertEquals(expected, actual);
                 }
 
 
